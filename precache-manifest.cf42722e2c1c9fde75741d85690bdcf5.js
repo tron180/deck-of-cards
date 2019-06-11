d39e0a357adf7985ab01fc7f56ee81056188c749
Yash Mishra <yash.mishra@raweng.com>
2019-06-11 12:50:01 +0530

    Updates

---
+++ precache-manifest.cf42722e2c1c9fde75741d85690bdcf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c06046dd2c426dd3863ec8a8b415d2",
+    "url": "/deck-of-cards/index.html"
+  },
+  {
+    "revision": "ad14e7f4871c05798458",
+    "url": "/deck-of-cards/static/css/main.e306f9ea.chunk.css"
+  },
+  {
+    "revision": "e7cea71c3eabc44783ad",
+    "url": "/deck-of-cards/static/js/2.b8f9b95f.chunk.js"
+  },
+  {
+    "revision": "ad14e7f4871c05798458",
+    "url": "/deck-of-cards/static/js/main.03a821e5.chunk.js"
+  },
+  {
+    "revision": "075a63fd01fb37d38b67",
+    "url": "/deck-of-cards/static/js/runtime~main.bce7395b.js"
+  }
+]);
